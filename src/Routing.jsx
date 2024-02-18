a08099f2cd6f392f921ca4a701d19afadad940f4
vinoth <vinoth@gmail.com>
2024-02-18 13:01:34 +0530

    updated

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,26 +1,20 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SubjectSelector from "./Component/SubjectSelector";
 import Testing from "./Component/testing";
-import Header from "./Component/Header/Header"
+import Header from "./Component/Header/Header";
 const Routing = () => {
   return (
-  
-<Router basename="/testingcollege.netlify.app">
-    <div>
-  
-      <Routes>
-        <Route path="/" element={<Header />} />
-        <Route
-          path="/department/:department"
-          element={<SubjectSelector />}
-        />
-        <Route path="/syllabus/:subjectCode" element={<Testing />} />
-      </Routes>
-    
-    </div>
-  </Router>
-  )
-}
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Header />} />
+          <Route path="/department/:department" element={<SubjectSelector />} />
+          <Route path="/syllabus/:subjectCode" element={<Testing />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-export default Routing
+export default Routing;
